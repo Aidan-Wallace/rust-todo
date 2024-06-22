c36c495df1d913099b33c3cbfe740b4e78d81d02
Aidan Wallace <77929240+Aidan-Wallace@users.noreply.github.com>
2024-06-21 23:12:57 -0400

    Update Cargo.toml (#6)

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,17 @@ mod todo;
 mod utils;
 
 use crate::todo::{add_todo, clear_todos, list_todos, remove_todo};
-use clap::{arg, Arg, ArgAction, Command};
+use clap::{Arg, ArgAction, Command};
 use db::{data_repository::DataRepo, database::Db, repository::Repository};
 use dotenv::dotenv;
 use std::env;
 use std::error::Error;
-use log4rs::append::file::FileAppender;
-use log4rs::Config;
-use log4rs::config::{Appender, Root};
-use log4rs::encode::pattern::PatternEncoder;
-use log::LevelFilter;
 
 const APP_NAME: &str = "rust-todos";
-const DATEBASE_NAME: &str = "todos.db";
 const DATE_FORMAT: &str = "%Y/%m/%d %H:%M:%S";
 
 fn main() {
     dotenv().ok();
-    setup_logger();
 
     let db_str = utils::get_db_string().expect("something went wrong when getting app data folder");
 
@@ -36,7 +29,7 @@ fn main() {
 
     let args = parse_args();
 
-    let mut load_fake_data = args.get_flag("fill");
+    let load_fake_data = args.get_flag("fill");
     db.setup(load_fake_data);
 
     let data_repo = DataRepo::new(db);
@@ -66,7 +59,7 @@ fn main() {
 
             remove_todo(&data_repo, parsed_integers);
         }
-        Some(("clear", sub_matches)) => {
+        Some(("clear", _)) => {
             clear_todos(&data_repo);
         }
         _ => list_todos(&data_repo),
@@ -104,22 +97,4 @@ fn parse_args() -> clap::ArgMatches {
         .get_matches();
 
     matches
-}
-
-fn setup_logger() -> Result<(), Box<dyn Error>> {
-    let logfile = FileAppender::builder()
-        .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
-        .build("log/output.log")?;
-
-    let config = Config::builder()
-        .appender(Appender::builder().build("logfile", Box::new(logfile)))
-        .build(Root::builder()
-            .appender("logfile")
-            .build(LevelFilter::Info))?;
-
-    log4rs::init_config(config)?;
-
-    log::info!("Hello, world!");
-
-    Ok(())
 }
