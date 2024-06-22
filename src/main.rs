81fe146950e8b93e82f9f1b23ce8f091acb52e9b
Aidan Wallace <77929240+Aidan-Wallace@users.noreply.github.com>
2024-06-21 22:43:57 -0400

    fake

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,20 @@ use clap::{arg, Arg, ArgAction, Command};
 use db::{data_repository::DataRepo, database::Db, repository::Repository};
 use dotenv::dotenv;
 use std::env;
+use std::error::Error;
+use log4rs::append::file::FileAppender;
+use log4rs::Config;
+use log4rs::config::{Appender, Root};
+use log4rs::encode::pattern::PatternEncoder;
+use log::LevelFilter;
 
 const APP_NAME: &str = "rust-todos";
+const DATEBASE_NAME: &str = "todos.db";
 const DATE_FORMAT: &str = "%Y/%m/%d %H:%M:%S";
 
 fn main() {
     dotenv().ok();
+    setup_logger();
 
     let db_str = utils::get_db_string().expect("something went wrong when getting app data folder");
 
@@ -96,4 +104,22 @@ fn parse_args() -> clap::ArgMatches {
         .get_matches();
 
     matches
+}
+
+fn setup_logger() -> Result<(), Box<dyn Error>> {
+    let logfile = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
+        .build("log/output.log")?;
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .build(Root::builder()
+            .appender("logfile")
+            .build(LevelFilter::Info))?;
+
+    log4rs::init_config(config)?;
+
+    log::info!("Hello, world!");
+
+    Ok(())
 }
